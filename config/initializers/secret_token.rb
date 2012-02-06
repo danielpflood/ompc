83104f07203ea4b14eba7a5e5b78bec6d94b2e3c
Daniel Flood <danielpflood@gmail.com>
2012-02-06 16:11:29 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ompc::Application.config.secret_token = 'dc17d42ba9c338772b8fd152d199ae4a33b4d70d9756adbc1b8f87329e49b2137bdd5499f1161d3ff2d3e915da1917989d5593f312264dd1779be2dae91d53c5'
